9327eab7a69c5d68f4af3569bbc94cd8f10b2f4c
Zuo Yang <zuya@microsoft.com>
2018-01-17 12:10:55 +0800

    URL rewriting in ASP.NET Core 2

--- Startup.cs
+++ Startup.cs
@@ -1,43 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.AspNetCore.Rewrite.Internal;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace ng_products
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace ng_products {
+
+    public class Startup {
+
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddMvc ();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
             }
 
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-            
-            app.UseMvc();
+            app.Use (async (context, next) => {
+                await next ();
+
+                if (context.Response.StatusCode == 404 && !Path.HasExtension (context.Request.Path.Value)) {
+                    context.Request.Path = "/";
+                    context.Response.StatusCode = 200;
+                    await next ();
+                }
+            });
+
+            app.UseDefaultFiles ();
+            app.UseStaticFiles ();
+
+            app.UseMvc ();
         }
     }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HowItWorkComponent } from './how-it-work/how-it-work.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'how-it-works', component: HowItWorkComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

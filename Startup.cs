c943a7c75ff48376e231f0d4fcfdc5634e5bad94
Zuo Yang <zuya@microsoft.com>
2018-01-17 12:13:56 +0800

    URL rewriting in ASP.NET Core 2

--- Startup.cs
+++ Startup.cs
@@ -12,31 +12,37 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace ng_products {
-
-    public class Startup {
-
-        public Startup (IConfiguration configuration) {
+namespace ng_products
+{
+    public class Startup
+    {
+        public Startup (IConfiguration configuration)
+        {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices (IServiceCollection services) {
+        public void ConfigureServices (IServiceCollection services)
+        {
             services.AddMvc ();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
-            if (env.IsDevelopment ()) {
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment ())
+            {
                 app.UseDeveloperExceptionPage ();
             }
 
-            app.Use (async (context, next) => {
+            app.Use (async (context, next) =>
+            {
                 await next ();
 
-                if (context.Response.StatusCode == 404 && !Path.HasExtension (context.Request.Path.Value)) {
+                if (context.Response.StatusCode == 404 && !Path.HasExtension (context.Request.Path.Value))
+                {
                     context.Request.Path = "/";
                     context.Response.StatusCode = 200;
                     await next ();

a0f73cdaace561856491870382cca696d0fed490
Zuo Yang <zuya@microsoft.com>
2018-01-17 11:15:06 +0800

    Add How-It-Work Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { ProductsService } from './common/products.service';
+import { HowItWorkComponent } from './how-it-work/how-it-work.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HowItWorkComponent
   ],
   imports: [
     BrowserModule,

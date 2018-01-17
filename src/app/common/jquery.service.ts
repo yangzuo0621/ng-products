45a7090966674075bed84ddc4b03f8c0ddf84a7b
Zuo Yang <zuya@microsoft.com>
2018-01-17 14:34:34 +0800

    Add jquery into angular

--- src/app/common/jquery.service.ts
+++ src/app/common/jquery.service.ts
@@ -1,3 +1,4 @@
 import { InjectionToken } from '@angular/core';
 
+// core: OpaqueToken has been removed as it was deprecated since v4. Use InjectionToken instead.
 export let JQUERY_TOKEN = new InjectionToken('jQuery');
